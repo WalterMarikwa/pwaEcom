0ac5c5d769965277aac94182618ff79d84811122
Walter Marikwa <waltermarikwa02@gmail.com>
2019-12-02 13:48:43 -0500

    fixing err

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ let uri = process.env.ATLAS_URI
 // register middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// force https for production
+// code sourced from https://stackoverflow.com/questions/8605720/how-to-force-ssl-https-in-express-js
+app.use((req, res, next) => {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === "production") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+})
 
 // routes require
 const Products = require('./routes/Products');
